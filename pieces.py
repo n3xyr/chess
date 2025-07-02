a1ff440dbb9a137ada45ce309da0b8edd0678a5d
XeroXII <160334817+XeroXII@users.noreply.github.com>
2025-07-02 14:28:07 +0200

    init pawn

---
+++ pieces.py
@@ -0,0 +1,40 @@
+class pawn:
+    def __init__(self, coordX, coordY, color):
+        self.__coordinateX = coordX
+        self.__coordinateY = coordY 
+        self.__color = color
+
+    def getCoordX(self):
+        """
+        returns pawn's x coordinate
+        """
+        return self.__coordinatesX
+    
+    def getCoordY(self):
+        """
+        returns pawn's y coordinate
+        """
+        return self.__coordinatesY
+    
+    def getColor(self):
+        """
+        returns pawn's color
+        """
+        return self.__color
+
+    def setCoordX(self, x):
+        self.__coordinateX = x
+
+    def setCoordY(self, y):
+        self.__coordinateY = y
+
+    def isFirstMove(self):
+        """
+        returns True if the pawn hasn't moved yet False otherwise
+        """
+        if self.getColor() == 'black' and self.getCoordY() == 6:
+            return True
+        if self.getColor() == 'white' and self.getCoordY() == 1:
+            return True
+        return False
+
