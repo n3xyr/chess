646fba22eaf5ad9db846cacaccd1f0bf9903ee96
n3xyr <joris.cavignac@gmail.com>
2025-09-21 01:34:55 +0200

    feat: original settings values are gettable

--- globals.py
+++ globals.py
@@ -3,3 +3,13 @@ showSettings = False
 settingsButtonsDrawn = False
 showPossibleMovesSwitchState = True
 disableSoundsSwitchState = False
+
+def getSettingsOriginalValues():
+    settingsOriginalValues = {
+    "primaryColor" : "739552",
+    "secondaryColor" : "ebecd0",
+    "pieceChoice" : "Neo",
+    "showPossibleMoves" : True,
+    "disableSounds" : False
+    }
+    return settingsOriginalValues
